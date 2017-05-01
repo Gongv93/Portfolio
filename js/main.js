e44a23c2e739609fae97659c3b276825ede2276e
Vincent Gong <gongv93@gmail.com>
2017-05-01 12:59:13 -0400

    Update skill bar to animate when scrolled to

--- js/main.js
+++ js/main.js
@@ -4,24 +4,63 @@ var SCROLL_TIME      = 400;       // Animation time for scrolling
 
 $(document).ready(function()
 {
-    // Initialize the selected nav menu
-    var selectOffset = $("li").first().offset().top;
-    document.getElementById("active").style.top = selectOffset;
-    $("li").first().addClass("active2");
-
+/**********************************************
+ *                  Listeners
+ **********************************************/
     // Load the main content to the page
     // 
     $("#main").load("main.html", function() {
+       // Initialize the selected nav menu
+       var selectOffset = $("li").first().offset().top;
+       $("#active").css('top', selectOffset)
+       $("li").first().addClass("active2");
+
        // Get git hub repo information and create links to them
        $.getJSON("https://api.github.com/users/Gongv93/starred", createProjectTiles);
+    });
 
-       $('.skillbar').each(function(){
-            $(this).find('.skillbar-bar').animate({
-                width: $(this).attr('data-percent')
-            },6000);
+    // Add smooth scroll to all links
+    // 
+    $("nav a").click(function(event) {
+        // Prevent default behavior 
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        $(".active2").removeClass("active2");
+        $(this).addClass("active2");
+
+        // Scroll for 8ms to the specified location
+        $('html, body').animate({
+             scrollTop: $(hash).offset().top
+        }, { duration: SCROLL_TIME, 
+             queue: false, 
+             complete: function() {
+                window.location.hash = hash;    // Set the new hash location
+            }
         });
     });
 
+    // Scroll listener
+    //
+    $(window).scroll(function () {
+        checkAnimation();
+        updateNav();
+    });
+
+    // Toggle button for showing nav
+    //
+    $("#toggleNav").click(function() {
+        $("#sidebar").toggleClass("toggle toggleOff");
+        $("#main").toggleClass("toggle toggleOff");
+        $("#toggleNav").toggleClass("toggle toggleOff");
+        $("#footer").toggleClass("toggle toggleOff");
+    });
+
+/**********************************************
+ *                  Functions 
+ **********************************************/
     // This function takes the git information and creates our project tiles to display
     // 
     function createProjectTiles(gitInfo) {
@@ -77,42 +116,8 @@ $(document).ready(function()
         }
     }
 
-    // Add smooth scroll to all links
-    // 
-    $("nav a").click(function(event) {
-        // Prevent default behavior 
-        event.preventDefault();
-
-        // Store hash
-        var hash = this.hash;
-
-        $(".active2").removeClass("active2");
-        $(this).addClass("active2");
-
-        // Scroll for 8ms to the specified location
-        $('html, body').animate({
-             scrollTop: $(hash).offset().top
-        }, { duration: SCROLL_TIME, 
-             queue: false, 
-             complete: function() {
-                window.location.hash = hash;    // Set the new hash location
-            }
-        });
-    });
-
-    $(window).scroll(function () {
-        updateNav();
-    });
-
-    // Toggle button for showing nav
+    // This function updates the nav when the user scrolls
     //
-    $("#toggleNav").click(function() {
-        $("#sidebar").toggleClass("toggle toggleOff");
-        $("#main").toggleClass("toggle toggleOff");
-        $("#toggleNav").toggleClass("toggle toggleOff");
-        $("#footer").toggleClass("toggle toggleOff");
-    });
-
     function updateNav() {
         var threshold_line = 50;   // Number of pixel from the top before selection gets updated
 
@@ -141,5 +146,46 @@ $(document).ready(function()
                 });
             }
         });
+    }
+
+    // Check if it's time to start the animation.
+    //
+    function checkAnimation() {
+        for(i=0; i < $('.skillbar').length; ++i) {
+            var $elem = $($('.skillbar')[i]);
+
+            // If the animation has already been started
+            if ($elem.hasClass('start')) continue;
+
+            if (isElementInViewport($elem)) {
+                console.log("hi");
+                var percent = $elem.attr('data-percent');
+                $elem.find('.skillbar-bar').animate({
+                    width: percent
+                },{ duration: 6000, 
+                    queue: false
+                });
+
+                // Start the animation
+                $elem.addClass('start');
+            }
+        }
+    }
+
+    // Returns true if the specified element has been scrolled into the viewport.
+    //
+    function isElementInViewport(elem) {
+        var $elem = $(elem);
+
+        // Get the scroll position of the page.
+        var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+        var viewportTop = $(scrollElem).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        // Get the position of the element on the page.
+        var elemTop = Math.round( $elem.offset().top );
+        var elemBottom = elemTop + $elem.height();
+
+        return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
     }
 });
